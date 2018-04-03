cde9344c0ee6b125945339e407b4a77897be615c
Daniel Chan <dc3033@nyu.edu>
2018-04-03 12:16:21 -0400

    Initial commit
    Moving files to laptop to test controller support

---
+++ Prototype 7/Assets/Blue2Control.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blue2Control : MonoBehaviour {
+
+    public float speed;
+    private float climbspeed;
+    private float moveY;
+
+    Rigidbody2D rb;
+
+    private bool hasGem = false;
+    private bool cantBomb = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal2");
+        moveY = Input.GetAxis("Vertical2");
+
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if ((collision.gameObject.CompareTag("Ladder")) || (collision.gameObject.CompareTag("Rope") && hasGem == false))
+        {
+            climbspeed = 3;
+            rb.gravityScale = 0;
+            rb.velocity = new Vector2(rb.velocity.x, moveY * climbspeed);
+        }
+        else if (collision.gameObject.CompareTag("Rope") && hasGem)
+        {
+            climbspeed = 1.5f;
+            rb.gravityScale = 0;
+            rb.velocity = new Vector2(rb.velocity.x, moveY * climbspeed);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder") || collision.gameObject.CompareTag("Rope"))
+        {
+            climbspeed = 0;
+            rb.gravityScale = 2;
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+    }
+}
---
+++ Prototype 7/Assets/PCG_script.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCG_script : MonoBehaviour
+{
+
+    public GameObject laserCannon;
+    public GameObject laser;
+    public GameObject ladder;
+    public GameObject supply;
+    public GameObject gateSwitch;
+    public GameObject floor;
+
+    //spacing between floors is 2
+    //ladder is 2 ladder objects stacked on top of each other, ypos for higher ladder is 1 more than lower ladder, ypos for lower ladder is same as other objects
+    private float[] fxpos = { -5.5f, -2.5f, 2.5f, 5.5f };
+    private float[] oxpos = { -4.5f, -1.5f, 1.5f, 4.5f };
+    private float l1fypos = 5.73f;
+    private float l1oypos = 4.5f;
+
+    private int emptyCount = 0;
+    private int cannonCount = 0;
+    private int switchCount = 0;
+    private int supplyCount = 0;
+
+    void fillMap()
+    {
+        //loop through each floor
+        for (int i = 0; i < 6; i++)
+        {
+            //choose position of ladder on the floor
+            int lpos = Random.Range(0, 4);
+            for (int j = 0; j < 4; j++)
+            {
+                if (lpos == j)
+                {
+                    var ladlow = Instantiate(ladder, new Vector3(fxpos[j], l1oypos - 2 * i, 0), Quaternion.identity);
+                    var ladhigh = Instantiate(ladder, new Vector3(fxpos[j], l1oypos - 2 * i + 1, 0), Quaternion.identity);
+                }
+                else
+                {
+                    var nolad = Instantiate(floor, new Vector3(fxpos[j], l1fypos - 2 * i, 0), Quaternion.identity);
+                }
+            }
+
+            //add objects to be randomly chosen, 1 = empty, 2 = lasercannon, 3 = switch, 4 = supply
+            List<int> choices = new List<int>();
+            if (emptyCount < 4) choices.Add(1);
+            if (cannonCount < 4) choices.Add(2);
+            if (switchCount < 2) choices.Add(3);
+            if (supplyCount < 2) choices.Add(4);
+
+            //place first object
+            int firstpick = choices[Random.Range(0, choices.Count)];
+            int firstpos = Random.Range(0, 2);
+            if (firstpick == 1)
+            {
+                emptyCount += 1;
+            }
+            else if (firstpick == 2)
+            {
+                cannonCount += 1;
+                choices.Remove(2);
+                var canInstance = Instantiate(laserCannon, new Vector3(oxpos[firstpos], l1oypos - 2 * i, 0), Quaternion.identity);
+                canInstance.transform.Rotate(0, 0, -90);
+                if (Random.Range(0, 2) == 0)
+                {
+                    var laserleft = Instantiate(laser, new Vector3(oxpos[firstpos] - 8, l1oypos - 2 * i, 0), Quaternion.identity);
+                }
+                else
+                {
+                    Vector3 theScale = canInstance.transform.localScale;
+                    theScale.y *= -1;
+                    canInstance.transform.localScale = theScale;
+                    var laserright = Instantiate(laser, new Vector3(oxpos[firstpos] + 8, l1oypos - 2 * i, 0), Quaternion.identity);
+                }
+            }
+            else if (firstpick == 3)
+            {
+                switchCount += 1;
+                choices.Remove(3);
+                var switchInstance = Instantiate(gateSwitch, new Vector3(oxpos[firstpos], l1oypos - 2 * i, 0), Quaternion.identity);
+            }
+            else if (firstpick == 4)
+            {
+                supplyCount += 1;
+                choices.Remove(4);
+                var supplyInstance = Instantiate(supply, new Vector3(oxpos[firstpos], l1oypos - 2 * i, 0), Quaternion.identity);
+            }
+
+            //place second object
+            int secondpick = choices[Random.Range(0, choices.Count)];
+            int secondpos = Random.Range(2, 4);
+            if (secondpick == 1)
+            {
+                emptyCount += 1;
+            }
+            else if (secondpick == 2)
+            {
+                cannonCount += 1;
+                choices.Remove(2);
+                var canInstance = Instantiate(laserCannon, new Vector3(oxpos[secondpos], l1oypos - 2 * i, 0), Quaternion.identity);
+                canInstance.transform.Rotate(0, 0, -90);
+                if (Random.Range(0, 2) == 0)
+                {
+                    var laserleft = Instantiate(laser, new Vector3(oxpos[secondpos] - 8, l1oypos - 2 * i, 0), Quaternion.identity);
+                }
+                else
+                {
+                    Vector3 theScale = canInstance.transform.localScale;
+                    theScale.y *= -1;
+                    canInstance.transform.localScale = theScale;
+                    var laserright = Instantiate(laser, new Vector3(oxpos[secondpos] + 8, l1oypos - 2 * i, 0), Quaternion.identity);
+                }
+            }
+            else if (secondpick == 3)
+            {
+                switchCount += 1;
+                choices.Remove(3);
+                var switchInstance = Instantiate(gateSwitch, new Vector3(oxpos[secondpos], l1oypos - 2 * i, 0), Quaternion.identity);
+            }
+            else if (secondpick == 4)
+            {
+                supplyCount += 1;
+                choices.Remove(4);
+                var supplyInstance = Instantiate(supply, new Vector3(oxpos[secondpos], l1oypos - 2 * i, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        fillMap();
+    }
+
+}
